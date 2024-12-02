8c8d5607b0055c2d32c1c6c02b9fdf4445b6d3ee
Dharmesh <raikwardharmesh67@gmail.com>
2024-12-02 21:52:17 +0530

    helo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { SignInButton, SignedOut, SignedIn, UserButton } from '@clerk/clerk-reac
 import Button1 from '../utils/Button1';
 const Home = () => {
     return (
-        <div className='h-[95vh]'>
+        <div className='h-[92vh]'>
             <header className='fixed top-0 left-0 right-0 bg-slate-950 text-white p-4 flex justify-between items-center z-10'>
                 <div>
                     <img src={sidelogo} alt="Logo" className="h-8 w-auto" />
